8ed36a03abc3cb61c17cd512bc1b25f4ae4a6d79
Alexander Rodgers <alexedrodgers@gmail.com>
2019-12-24 14:18:41 -0800

    Updated sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 import TextField from '@material-ui/core/TextField';
-import { CardContent } from '@material-ui/core';
 
 const LoginCard = styled(Card)`
 width: 50%;
@@ -10,27 +10,90 @@ margin: auto;
 height: 50%;
 `;
 
+const SignUpTextField = styled(TextField)`
+width: 100%
+margin: 0 10px 0 10px;
+`;
+
+const formSpacing = {
+   margin: '0 10px 0 10px'
+};
 
 class SignUp extends Component {
    constructor(props) {
       super(props);
+      this.state = { firstName: '', lastName: '', email: '', password: '', submitDisabled: true};
    }
+
    handleChange = (stateProp, event) => {
-      this.setState({[stateProp]: event.target.value});
+      this.setState({[stateProp]: event.target.value}, () => {
+         // Callback function
+         this.isDisabled();
+      });
    }
+
+   submitForm = () => {
+      console.log('submitting.');
+   }
+
+   isDisabled = () => {
+      if (!!this.state.email && !!this.state.firstName && !!this.state.lastName && !!this.state.password) {
+         this.setState({submitDisabled: false});
+         return;
+      }
+      this.setState({submitDisabled: true});
+   }
+
    render() {
       return (
          <LoginCard>
             <form>
-               <div>
-               <TextField
-                  id="outlined-basic"
-                  label="Outlined"
-                  margin="normal"
-                  variant="outlined"
-               />
+               <div style={formSpacing}>
+                  <SignUpTextField
+                     id="outlined-basic"
+                     label="First Name"
+                     margin="normal"
+                     onChange={(event) => this.handleChange('firstName', event)}
+                     variant="outlined"
+                     required
+                  />
                </div>
-            </form>   
+               <div style={formSpacing}>
+                  <SignUpTextField
+                     id="outlined-basic"
+                     label="Last Name"
+                     margin="normal"
+                     onChange={(event) => this.handleChange('lastName', event)}
+                     variant="outlined"
+                     required
+                  />
+               </div>
+               <div style={formSpacing}>
+                  <SignUpTextField
+                     id="outlined-basic"
+                     label="Email"
+                     onChange={(event) => this.handleChange('email', event)}
+                     margin="normal"
+                     variant="outlined"
+                     required
+                  />
+               </div>
+               <div style={formSpacing}>
+                  <SignUpTextField
+                     id="outlined-password-input"
+                     label="Password"
+                     value={this.state.password}
+                     onChange={(event) => this.handleChange('password', event)}
+                     type="password"
+                     autoComplete="current-password"
+                     margin="normal"
+                     variant="outlined"
+                     required
+                  />
+               </div>
+               <Button variant="outlined" color="primary" onClick={() => this.submitForm()}
+               style={{margin: "0 10px 0 10px", float: 'right'}} disabled={this.state.submitDisabled}>Submit</Button>
+            </form>
          </LoginCard>
       )
    }
